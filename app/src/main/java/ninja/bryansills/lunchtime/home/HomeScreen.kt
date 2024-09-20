87b2243e8765e46222747655709b27f48b23b4ff
bryansills <bryan@bryansills.com>
2024-09-19 22:48:38 -0500

    Got the rough UI for the list down

--- app/src/main/java/ninja/bryansills/lunchtime/home/HomeScreen.kt
+++ app/src/main/java/ninja/bryansills/lunchtime/home/HomeScreen.kt
@@ -8,19 +8,25 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -28,9 +34,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
@@ -48,6 +57,8 @@ data object Home
 fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel(),
 ) {
+    val uiState by viewModel.uiState.collectAsState()
+
     WithLocation {
         Scaffold(
             modifier = Modifier.fillMaxSize()
@@ -58,6 +69,11 @@ fun HomeScreen(
                 val selectedView by rememberSaveable { mutableStateOf(HomeScreenView.List) }
 
                 Box(modifier = Modifier.fillMaxWidth().weight(1f)) {
+                    when (selectedView) {
+                        HomeScreenView.List -> HomeList(uiState.searchResults)
+                        HomeScreenView.Map -> TODO()
+                    }
+
                     BottomToggleButton(
                         selectedView = selectedView,
                         onClick = {},
@@ -117,6 +133,86 @@ private fun BottomToggleButton(
                 contentDescription = stringResource(selectedView.oppositeTitle),
             )
             Text(text = stringResource(selectedView.oppositeTitle))
+        }
+    }
+}
+
+@Composable
+private fun HomeList(restaurants: List<UiRestaurant>, modifier: Modifier = Modifier) {
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 24.dp, vertical = 32.dp), // TODO: validate
+        verticalArrangement = Arrangement.spacedBy(24.dp), // TODO: validate
+        modifier = modifier
+    ) {
+        items(restaurants) { item: UiRestaurant ->
+            RestaurantItem(item)
+        }
+    }
+}
+
+@Composable
+private fun RestaurantItem(item: UiRestaurant, modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(16.dp), // TODO: validate
+        shadowElevation = 4.dp, // TODO: validate
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp), // TODO: validate
+            horizontalArrangement = Arrangement.spacedBy(16.dp), // TODO: validate
+        ) {
+            Image(
+                painter = painterResource(R.drawable.placeholder_img), // TODO: real value with coil
+                contentDescription = "Preview image for ${item.name}",
+                contentScale = ContentScale.Crop,
+                alignment = Alignment.Center,
+                modifier = Modifier.size(width = 64.dp, height = 72.dp),
+            )
+
+            Column {
+                Row {
+                    Text(
+                        text = item.name,
+                        style = MaterialTheme.typography.labelLarge, // TODO: all the typography
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.weight(1f),
+                    )
+
+                    val resId = if (item.isBookmarked) R.drawable.bookmark_saved else R.drawable.bookmark_resting
+                    Image(
+                        painter = painterResource(resId),
+                        contentDescription = if (item.isBookmarked) "Bookmarked" else "Not bookmarked",
+                        modifier = Modifier
+                            .padding(start = 8.dp), // TODO: validate
+                    )
+                }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(
+                        painter = painterResource(R.drawable.icon_star),
+                        contentDescription = "Star rating",
+                    )
+                    Text(
+                        text = "%.1f".format(Locale.current.platformLocale, item.rating),
+                        style = MaterialTheme.typography.labelLarge, // TODO: all the typography
+                    )
+                    Text(
+                        text = "•",
+                        style = MaterialTheme.typography.labelLarge, // TODO: all the typography
+                    )
+                    Text(
+                        text = "%,d".format(Locale.current.platformLocale, item.numOfRatings),
+                        style = MaterialTheme.typography.labelLarge, // TODO: all the typography
+                    )
+                }
+                Text(
+                    text = item.city,
+                    style = MaterialTheme.typography.labelLarge, // TODO: all the typography
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            }
         }
     }
 }
--- app/src/main/java/ninja/bryansills/lunchtime/home/HomeViewModel.kt
+++ app/src/main/java/ninja/bryansills/lunchtime/home/HomeViewModel.kt
@@ -2,8 +2,43 @@ package ninja.bryansills.lunchtime.home
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor() : ViewModel() {
+    val uiState: StateFlow<HomeUiState> = MutableStateFlow(
+        HomeUiState(
+            isLoading = false,
+            currentLocation = 0f to 0f,
+            selectedRestaurant = null,
+            searchResults = (0..20).map { index ->
+                UiRestaurant(
+                    id = index.toString(),
+                    name = "Fake Restaurant",
+                    rating = 5.0f,
+                    numOfRatings = 1000,
+                    city = "Chicago",
+                    isBookmarked = false,
+                )
+            }
+        )
+    )
 }
+
+data class HomeUiState(
+    val isLoading: Boolean,
+    val currentLocation: Pair<Float, Float>,
+    val selectedRestaurant: UiRestaurant?,
+    val searchResults: List<UiRestaurant>
+)
+
+data class UiRestaurant(
+    val id: String,
+    val name: String,
+    val rating: Float,
+    val numOfRatings: Int,
+    val city: String,
+    val isBookmarked: Boolean,
+)
